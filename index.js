a5da28c873d14f282cc22462135c5a42d604748b
Paulo Diovani Gonçalves <paulo@diovani.com>
2016-03-29 14:55:43 -0300

    Add example use on index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var Q       = require('q');
+
+var express = require('express');
+var app     = express();
+
+var router  = require('./lib/promise-router')(app);
+
+router.get('/', function(req, res) {
+  // create and defer a promise
+  var deferred = Q.defer();
+  setTimeout(function() {
+    deferred.resolve('Hello, World.');
+  }, 3000);
+
+  // now promises can be chained and
+  // the last resolved promise data
+  // will be sent do response
+  return deferred.promise;
+});
+
+app.set('port', 3000);
+app.listen(app.get('port'), function() {
+  console.log('Server listening on port ' + app.get('port'));
+});
